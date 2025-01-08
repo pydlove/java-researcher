e40d8e6bd59a13725fb949b2854a09c0997f6e3a
panyong <panyong@secsmart.net>
2025-01-08 09:45:53 +0800

    feat:// api-gateway cache 实现

--- api-gateway/gateway-cache/src/main/java/com/aiocloud/gateway/cache/core/CacheManager.java
+++ api-gateway/gateway-cache/src/main/java/com/aiocloud/gateway/cache/core/CacheManager.java
@@ -2,6 +2,7 @@ package com.aiocloud.gateway.cache.core;
 
 import com.github.benmanes.caffeine.cache.Cache;
 import com.github.benmanes.caffeine.cache.Caffeine;
+import lombok.extern.slf4j.Slf4j;
 
 import java.util.concurrent.TimeUnit;
 
@@ -14,11 +15,16 @@ import java.util.concurrent.TimeUnit;
  * @version: 1.0.0
  * @createTime: 2025-01-03 17:42
  */
+@Slf4j
 public class CacheManager {
 
     private static final Cache<String, Object> cache = Caffeine.newBuilder()
             .expireAfterWrite(10, TimeUnit.MINUTES)
+            .expireAfterAccess(10, TimeUnit.MINUTES)
             .maximumSize(1000)
+            .removalListener((key, value, cause) -> {
+                log.info("key: {} was removed, cause: {}", key, cause);
+            })
             .build();
 
     public static void put(String key, Object value) {
@@ -27,5 +33,13 @@ public class CacheManager {
 
     public static Object get(String key) {
         return cache.getIfPresent(key);
+    }
+
+    public static void remove(String key) {
+        cache.invalidate(key);
+    }
+
+    public static void clear() {
+        cache.invalidateAll();
     }
 }
